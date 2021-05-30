cc140ea3b5fd134f843d1dea5d0cbfa4b315cc73
supriya-kotturu <supriyakotturu@pm.me>
2021-05-30 13:23:29 +0530

    add : token

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   API_NAME: "Github",
   API_URL: "https://api.github.com",
-  API_TOKEN: "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",
+  API_TOKEN: "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",
 };
 
 export default CONFIG;
